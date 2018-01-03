a0adaa5ab98e74abaf72b98cf3b4c3e37b07c25d
Ambjörn Fagerström <ambjorn89@gmail.com>
2018-01-03 18:21:15 +0100

    add all art

--- src/Art.js
+++ src/Art.js
@@ -32,6 +32,62 @@ export class ArtFlow extends Component {
         image: "/images/konst/bild-9.jpg",
         title: "Têtes 1970",
       },
+      {
+        image: "/images/konst/bild-11.jpg",
+        title: "l´Oiseau pris par la patte 1973",
+      },
+      {
+        image: "/images/konst/bild-12.jpg",
+        title: "La main grossissante 1973",
+      },
+      {
+        image: "/images/konst/bild-13.jpg",
+        title: "l´Homme prudent 1973",
+      },
+      {
+        image: "/images/konst/bild-10.jpg",
+        title: "l'Homme 1994",
+      },
+      {
+        image: "/images/konst/bild-14.jpg",
+        title: "Le jaune d´oeuf digital 1975",
+      },
+      {
+        image: "/images/konst/bild-15.jpg",
+        title: "Le sens sans sens avec un sens dense 1974",
+      },
+      {
+        image: "/images/konst/bild-16.jpg",
+        title: "Le jaune d´oeuf crânien et le jaune d´oeuf carré 1975",
+      },
+      {
+        image: "/images/konst/bild-17.jpg",
+        title: "l´Aller te demande avant que le Retour ne le fasse 1975",
+      },
+      {
+        image: "/images/konst/bild-18.jpg",
+        title: "La croix ombilicale n´agite guère 1975",
+      },
+      {
+        image: "/images/konst/bild-19.jpg",
+        title: "En chute libre 1977",
+      },
+      {
+        image: "/images/konst/bild-22.jpg",
+        title: "Des anges 2006",
+      },
+      {
+        image: "/images/konst/bild-23.jpg",
+        title: "Violons d´Ingres 2008",
+      },
+      {
+        image: "/images/konst/les-voisimes.jpg",
+        title: "Les voisines 2007",
+      },
+      {
+        image: "/images/konst/bild-25.jpg",
+        title: "Universums inre rymder 1992",
+      }
     ];
 
     const list = item.map(({ image, title }, i) =>
