6948835fb9e7afb2e79eb3abdd9d81733c460339
Ambjörn Fagerström <ambjorn.fagerstrom@fast2.se>
2021-06-15 20:42:03 +0200

    New photos

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import { Navigation } from './components/Navigation';
-import { Footer } from './components/Footer';
-import { Error } from './components/Error';
-import { Aterblick } from './components/Aterblick';
-import { Paris } from './components/Paris';
-import { Books } from './components/Books';
-import { Sculpture } from './components/Sculpture';
-import { Bio } from './components/Bio';
-import { Arboretum } from './components/Arboretum';
-import { Tools } from './components/Tools';
+import { Navigation } from "./components/Navigation";
+import { Footer } from "./components/Footer";
+import { Error } from "./components/Error";
+import { Aterblick } from "./components/Aterblick";
+import { Paris } from "./components/Paris";
+import { Books } from "./components/Books";
+import { Sculpture } from "./components/Sculpture";
+import { Bio } from "./components/Bio";
+import { Arboretum } from "./components/Arboretum";
+import { Tools } from "./components/Tools";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-  render() {
-
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Navigation />
-          <Switch>
-            <Route path={"/"} component={Bio} exact />
-            <Route path={"/aterblick"} component={Aterblick} />
-            <Route path={"/paristiden"} component={Paris} />
-            <Route path={"/arboretum"} component={Arboretum} />
-            <Route path={"/skulpturer"} component={Sculpture} />
-            <Route path={"/surplus"} component={Tools} />
-            <Route path={"/bocker"} component={Books} />
-            <Route component={Error} />
-          </Switch>
-          <Footer />
-        </div>
-      </BrowserRouter>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="App">
+                    <Navigation />
+                    <Switch>
+                        <Route path={"/"} component={Bio} exact />
+                        <Route path={"/aterblick"} component={Aterblick} />
+                        <Route path={"/paristiden"} component={Paris} />
+                        <Route path={"/arboretum"} component={Arboretum} />
+                        <Route path={"/skulpturer"} component={Sculpture} />
+                        <Route path={"/lartabstrait"} component={Tools} />
+                        <Route path={"/bocker"} component={Books} />
+                        <Route component={Error} />
+                    </Switch>
+                    <Footer />
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,35 +1,51 @@
 import React, { Component } from "react";
-import data from "./../data"; 
-import { NavLink } from 'react-router-dom';
+import data from "./../data";
+import { NavLink } from "react-router-dom";
 
 export class Navigation extends Component {
-  render() {
-    
-    const checkActive = (match, location) => {
-      //some additional logic to verify you are in the home URI
-      if(!location) return false;
-      const {pathname} = location;
-      return pathname === "/";
-    }
+    render() {
+        const checkActive = (match, location) => {
+            //some additional logic to verify you are in the home URI
+            if (!location) return false;
+            const { pathname } = location;
+            return pathname === "/";
+        };
 
-    return ( 
-      <nav>
-        { data.Header.map((header, i) => (
-            <div key={i}>
-              <h1 className="header__title">{header.title}</h1>
-              <ul className="navbar">
-                <li><NavLink to="/" isActive={checkActive}>Biografi</NavLink></li> 
-                <li><NavLink to="/aterblick">Återblick</NavLink></li>
-                <li><NavLink to="/paristiden">Paristiden</NavLink></li>
-                <li><NavLink to="/skulpturer">Skulpturer</NavLink></li>
-                <li><NavLink to="/arboretum">Arboretum</NavLink></li>
-                <li><NavLink to="/surplus">Surplus</NavLink></li>
-                <li><NavLink to="/bocker">Böcker</NavLink></li>
-              </ul>
-            </div>
-          )
-        )} 
-      </nav> 
-    );
-  }
+        return (
+            <nav>
+                {data.Header.map((header, i) => (
+                    <div key={i}>
+                        <h1 className="header__title">{header.title}</h1>
+                        <ul className="navbar">
+                            <li>
+                                <NavLink to="/" isActive={checkActive}>
+                                    Biografi
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/aterblick">Återblick</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/paristiden">Paristiden</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/skulpturer">Skulpturer</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/arboretum">Arboretum</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/lartabstrait">
+                                    L'art abstrait
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/bocker">Böcker</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                ))}
+            </nav>
+        );
+    }
 }
