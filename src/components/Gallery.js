9c0425bf967f87a0e39702dc9df16334f1432837
Ambjörn Fagerström <ambjorn89@gmail.com>
2022-12-04 13:25:01 +0100

    Remove lightbox

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,39 +1,23 @@
 import React, { Component } from 'react';
 
-import SimpleReactLightbox from 'simple-react-lightbox'; // Import Simple React Lightbox
-import { SRLWrapper } from 'simple-react-lightbox'; // Import SRLWrapper
-
 export class Gallery extends Component {  
   render() {
-
-    const options = {
-      transitionTimingFunction: "ease",
-      slideTransitionSpeed: 1000,
-      buttonsIconPadding: "2px",
-      enablePanzoom: false,
-      showThumbnails: false,
-      hideControlsAfter: 0
-    };
 
     return ( 
       <div className={this.props.class}>
         {this.props.children}
-        <SimpleReactLightbox>
-          <SRLWrapper options={options}>
-            <section>
-              { this.props.name.map((gallery, i) => {
-                return (
-                  <article key={i}>
-                    <div className="art-content">                    
-                      <img src={gallery.image} alt={gallery.title} />
-                      <p>{gallery.title}</p>
-                    </div>
-                  </article>
-                );
-              })}
-            </section>
-          </SRLWrapper>
-        </SimpleReactLightbox>
+          <section>
+            { this.props.name.map((gallery, i) => {
+              return (
+                <article key={i}>
+                  <div className="art-content">                    
+                    <img src={gallery.image} alt={gallery.title} />
+                    <p>{gallery.title}</p>
+                  </div>
+                </article>
+              );
+            })}
+          </section>
       </div>
     );
   }
